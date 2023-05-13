8ad0ba8af76584b87582d3813d2768c264c557bf
Thiago Figueiredo Costa <thiagofigcosta@gmail.com>
2023-05-13 02:14:37 -0300

    Update dijkstra.py

--- algo_data_design/algorithms/graph/dijkstra.py
+++ algo_data_design/algorithms/graph/dijkstra.py
@@ -76,7 +76,6 @@ def _shortest_paths_cost_with_priority_queue(graph, starting_node_or_data, desti
     # is the cost
     distances = {}  # store the distances from the starting node, if a node is not here, the distance is infinite
     visited = set() # elements should be added on queue at most once
-    # TODO should we should have a `visited` here, and only add to queue if not (mark as unvisited right before endind the while)?
     
     open_list.push(starting_node)  # start with the first node
     distances[starting_node] = 0  # there is not cost of staying in the same place
